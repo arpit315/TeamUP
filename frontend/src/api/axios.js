ca02f5aed9a8153527c5b24d0af96da609deb886
arpit <arpitkuma10@gmail.com>
2026-04-21 15:45:49 +0530

    fix api url template

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+
+const API = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
-    withCredentials: true, 
+    baseURL: `${API}/api/v1`,
+    withCredentials: true,
 });
+
 export default api;
